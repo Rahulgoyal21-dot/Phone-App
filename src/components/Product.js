68f2ddd67ccdfc6c4a96f0da763b9d3a84de2605
rahulgoyal21 <rahul.goyal@bitbucket.org>
2020-02-05 23:52:04 +0530

    Added:Product partially

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
-
+import styled from "styled-components";
+import { Box, Image } from "grommet";
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, info, inCart, company } = this.props.product;
     return (
-      <div>
-        <h3>hello from Product</h3>
-      </div>
+      <ProductWrapper fill="horizontal" direction="row">
+        <ProductWrapper
+          alignSelf="center"
+          justify-content="center"
+          height="200px"
+          width="200px"
+        >
+          <Image
+            fit="contain"
+            src={process.env.PUBLIC_URL + "img/product-1.png"}
+          />
+        </ProductWrapper>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled(Box)`
+  maxheight: medium;
+  overflow: auto;
+`;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,23 +1,24 @@
-import React, { Component, Fragment } from "react";
-import Product from "./Product";
+import React, { Component } from "react";
 import { Box } from "grommet";
 import { Title } from "./Title";
-import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
+import Product from "./Product";
 
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
   render() {
-    console.log(this.state.products);
     return (
       <React.Fragment>
         <Box>
           <Title name="Our" title="Products" />
           <ProductConsumer>
-            {hello => {
-              return <h1>{hello}</h1>;
+            {value => {
+              return (
+                <h1>
+                  {value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  })}
+                </h1>
+              );
             }}
           </ProductConsumer>
         </Box>
