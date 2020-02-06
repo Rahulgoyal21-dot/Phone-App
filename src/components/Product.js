fd9e3a67337de3c7ae7eafd1a3df53f9d9ada130
rahulgoyal21 <rahulgoyal21@bitbucket.org>
2020-02-06 17:27:56 +0530

    Removed ProductWrapper

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,28 +1,22 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import { Box, Image } from "grommet";
 export default class Product extends Component {
   render() {
     const { id, title, img, price, info, inCart, company } = this.props.product;
     return (
-      <ProductWrapper fill="horizontal" direction="row">
-        <ProductWrapper
-          alignSelf="center"
-          justify-content="center"
-          height="200px"
-          width="200px"
-        >
+      <Box direction="row" wrap gap="medium" flex="grow">
+        <Box basis="25%" minWidth="200px">
           <Image
             fit="contain"
             src={process.env.PUBLIC_URL + "img/product-1.png"}
           />
-        </ProductWrapper>
-      </ProductWrapper>
+        </Box>
+      </Box>
     );
   }
 }
 
-const ProductWrapper = styled(Box)`
-  maxheight: medium;
-  overflow: auto;
-`;
+// const ProductWrapper = styled(Box)`
+//   maxheight: medium;
+//   overflow: auto;
+// `;
